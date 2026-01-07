6cb870b6d513db616990ba5f60762e951ccb98ac
MohamedMoustir <itsmoustir@gmail.com>
2026-01-07 11:27:19 +0100

    feat: add time formatting pipe for audio duration

--- src/app/shared/pipes/format-time.pipe.ts
+++ src/app/shared/pipes/format-time.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'formatTime'
+  name: 'formatTime',
+  standalone: true
 })
 export class FormatTimePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(timeInSeconds: number): unknown {
+    if (!timeInSeconds || isNaN(timeInSeconds)) return '00:00';
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+
+    const paddingMinutes = minutes < 10 ? '0' : '';
+    const paddingSeconds = seconds < 10 ? '0' : '';
+
+    return `${paddingMinutes}${minutes}:${paddingSeconds}${seconds}`;
   }
 
 }
