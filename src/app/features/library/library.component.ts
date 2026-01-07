af74d2da9fd10fb314213258f699acb44a2e0453
MohamedMoustir <itsmoustir@gmail.com>
2026-01-07 18:24:07 +0100

    feat(library): enhance track selection and library logic

--- src/app/features/library/library.component.ts
+++ src/app/features/library/library.component.ts
@@ -1,27 +1,78 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormatTimePipe } from '../../shared/pipes/format-time.pipe';
 import { TrackService } from '../../core/services/track.service';
 import { AudioPlayerService } from '../../core/services/audio-player.service';
+import { FormatTimePipe } from '../../shared/pipes/format-time.pipe';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { Track } from '../../core/models/track';
+
 
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [CommonModule, RouterLink, FormatTimePipe],
+  imports: [CommonModule, RouterLink, FormatTimePipe, FormsModule],
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss'
 })
 export class LibraryComponent {
 
-  private trackService = inject(TrackService);
+  trackService = inject(TrackService);
   tracks$ = this.trackService.getAllTracks();
   playerService = inject(AudioPlayerService)
+  private coverCache = new Map<number, SafeUrl | string>();
+  private sanitizer = inject(DomSanitizer);
 
-   onPlay(track: any) {
-    console.log('✅ Clicked Track:', track);
-  console.log('📁 File Type:', track.file instanceof Blob);
-    this.playerService.playTrack(track);
+  searchQuery = signal<string>('');
+  selectedFilter = signal<string>('Tout');
+  private allTrack = signal<Track[]>([]);
+
+  filteredTracks = computed(() => {
+    const query = this.searchQuery().toLowerCase();
+    const filter = this.selectedFilter();
+    const tracks = this.allTrack();
+    return tracks.filter(track => {
+      const matchesSearch = track.title.toLowerCase().includes(query) ||
+        track.artist.toLowerCase().includes(query);
+
+      const matchesCategory = filter === 'Tout' || track.category === filter;
+
+      return matchesSearch && matchesCategory;
+    })
+  })
+
+  ngOnInit() {
+    this.trackService.getAllTracks().subscribe({
+      next: (data) => {
+        this.allTrack.set(data);
+      },
+      error: (err) => console.error('Erreur chargement tracks:', err)
+    })
+  }
+  onPlay(track: any) {
+    this.playerService.playTrack(track, this.filteredTracks());
+  }
+
+  getCoverUrl(track: any): SafeUrl | string {
+    if (!track.cover) return 'assets/images/placeholder.png';
+
+    if (typeof track.cover === 'string') return track.cover;
+
+    if (this.coverCache.has(track.id)) {
+      return this.coverCache.get(track.id)!;
+    }
+
+    if (track.cover instanceof Blob || track.cover instanceof File) {
+      const url = URL.createObjectURL(track.cover);
+      const safeUrl = this.sanitizer.bypassSecurityTrustUrl(url);
+
+      this.coverCache.set(track.id, safeUrl);
+
+      return safeUrl;
+    }
+
+    return '';
   }
   async deleteTrack(id: number, event: Event) {
     event.stopPropagation();
@@ -29,4 +80,27 @@ export class LibraryComponent {
       await this.trackService.deleteTrack(id);
     }
   }
+
+  onSeek(event: any) {
+    const value = event.target.value;
+    this.playerService.seekTo(value);
+  }
+
+  onVolume(event: any) {
+    const value = event.target.value;
+    this.playerService.setVolume(value);
+  }
+
+  previous(){
+         this.playerService.togglePlay();
+  }
+  
+  next(){
+         this.playerService.next();
+  }
+
+  togglePlay(){
+    this.playerService.togglePlay();
+  }
+
 }
