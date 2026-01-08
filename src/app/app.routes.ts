93b11f317191aa1a91b74f56b4b0297d1353283b
MohamedMoustir <itsmoustir@gmail.com>
2026-01-08 14:43:50 +0100

    chore(devops): add Dockerfile for containerization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { TrackManagerComponent } from './features/track-manager/track-manager.component';
 import { LibraryComponent } from './features/library/library.component';
+import { TrackDetailComponent } from './features/track-detail/track-detail.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'add-track', pathMatch: 'full' },
     { path: 'add-track', component: TrackManagerComponent },
     { path: 'library', component: LibraryComponent },
     { path: 'edit-track/:id', component: TrackManagerComponent },
-    { path: 'track/:id', component: TrackManagerComponent },
+    { path: 'track/:id', component: TrackDetailComponent },
     { path: '**', redirectTo: 'library' }
 
 
